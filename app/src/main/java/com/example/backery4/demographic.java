549d12bf734d97dabfe2a8f294e8ba0508b92f0d
SamsonJy <Samsonjy@gwu.edu>
2022-11-15 18:18:47 -0500

    seekBarCode Change

--- app/src/main/java/com/example/backery4/demographic.java
+++ app/src/main/java/com/example/backery4/demographic.java
@@ -13,14 +13,19 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+
 public class demographic extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private static final NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
     private TextView salaryRange;
     private SeekBar salarySeekBar;
-    private int currentSalary = 40000;
+    private int currentSalary = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demographic);
@@ -41,24 +46,32 @@ public class demographic extends AppCompatActivity implements AdapterView.OnItem
         spinner.setOnItemSelectedListener(this);
 
         salaryRange = findViewById(R.id.income_value);
+        salaryRange.setText(currencyFormat.format(0));
         salarySeekBar = findViewById(R.id.income_Bar);
-        salarySeekBar.setOnSeekBarChangeListener(seekBarListener);
+        salarySeekBar.setOnSeekBarChangeListener(salarySeekBarListener);
     }
-    private SeekBar.OnSeekBarChangeListener seekBarListener =
-            new SeekBar.OnSeekBarChangeListener() {
-                // update percent, then call calculate
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress,
-                                              boolean fromUser) {
-                    currentSalary = progress ;
-                }
+    private SeekBar.OnSeekBarChangeListener salarySeekBarListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            currentSalary = progress * 1000;
+            calculate();
+        }
 
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) { }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
 
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) { }
-            };
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private void calculate() {
+        salaryRange.setText(currencyFormat.format(currentSalary));
+    }
+
 
 
     @Override
