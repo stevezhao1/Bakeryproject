e419abb86f6891a859c63e4f7fb452c811eef7f6
stevezhao1 <99219516+stevezhao1@users.noreply.github.com>
2022-12-11 13:31:14 -0500

    New pages added

--- app/src/main/java/com/example/backery4/Grocerry_store.java
+++ app/src/main/java/com/example/backery4/Grocerry_store.java
@@ -2,7 +2,10 @@ package com.example.backery4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Grocerry_store extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Grocerry_store extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocerry_store);
+        Button safeway =(Button) findViewById(R.id.safeway);
+        safeway.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Grocerry_store.this, safeway.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/backery4/safeway.java
@@ -0,0 +1,14 @@
+package com.example.backery4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class safeway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safeway);
+    }
+}
